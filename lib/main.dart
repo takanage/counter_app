fd305a46f7b3608dd69fa4fddd1faa348d7af60a
Willian Takanage <williantakanage@gmail.com>
2021-06-20 22:33:47 -0300

    Created StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Container(
-      color: Colors.purple,
+    MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
     ),
   );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu Primeiro App"),
+      ),
+      body: Center(
+        child: Text(
+          "CONTADOR\n$count",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
 }
